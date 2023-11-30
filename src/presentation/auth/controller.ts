b9a128def6f3659cfb717388f3155f7f5f09a687
Francisco Lino <franksuriel_7@hotmail.com>
2023-11-30 01:48:13 -0600

    Use auth repository

--- src/presentation/auth/controller.ts
+++ src/presentation/auth/controller.ts
@@ -1,15 +1,19 @@
 import { Request, Response } from "express";
 import { RegisterUserDto } from "../../domain";
+import { AuthRepository } from "../../domain/repositories/auth.repository";
 
 export class AuthController {
-  constructor() {}
+  constructor(private readonly authRepository: AuthRepository) {}
 
   registerUser = (req: Request, res: Response) => {
     const [error, registerUserDto] = RegisterUserDto.create(req.body);
     if (error) {
       res.status(400).json({ error });
     }
-    res.json(registerUserDto);
+    return this.authRepository
+      .register(registerUserDto!)
+      .then((user) => res.json(user))
+      .catch((error) => res.status(500).json(error));
   };
 
   loginUser = (req: Request, res: Response) => {
--- src/presentation/auth/routes.ts
+++ src/presentation/auth/routes.ts
@@ -1,10 +1,13 @@
 import { Router } from "express";
 import { AuthController } from "./controller";
+import { AuthDatasourceImpl, AuthRepositoryImpl } from "../../infraestructure";
 
 export class AuthRoutes {
   static get routes(): Router {
     const router = Router();
-    const controller = new AuthController();
+    const datasource = new AuthDatasourceImpl();
+    const authRepository = new AuthRepositoryImpl(datasource);
+    const controller = new AuthController(authRepository);
 
     router.get("/login", controller.loginUser);
     router.post("/register", controller.registerUser);
