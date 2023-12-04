b82563281dfde2e781318c937f8ee7af0cb2a3b4
Francisco Lino <franksuriel_7@hotmail.com>
2023-12-04 02:39:19 -0600

    FIX duration type oarameter

--- src/config/jwt.ts
+++ src/config/jwt.ts
@@ -1,7 +1,7 @@
 import jwt from "jsonwebtoken";
 
 export class JwtAdapter {
-  static async generateToken(payload: Object, duration: "2h") {
+  static async generateToken(payload: Object, duration = "2h") {
     return new Promise((resolve) => {
       jwt.sign(payload, "SEED", { expiresIn: duration }, (err, token) => {
         if (err) return resolve(null);
