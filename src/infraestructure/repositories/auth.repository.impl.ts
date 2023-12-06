f5c6416fb9f644d5055335cb6be425ff563dc547
Francisco Lino <franksuriel_7@hotmail.com>
2023-12-06 00:58:51 -0600

    ADD implement repository

--- src/infraestructure/repositories/auth.repository.impl.ts
+++ src/infraestructure/repositories/auth.repository.impl.ts
@@ -1,8 +1,12 @@
 import { AuthRepository, RegisterUserDto, UserEntity } from "../../domain";
 import { AuthDatasource } from "../../domain/datasources/auth.datasource";
+import { LoginUserDto } from "../../domain/dtos/auth/login-user.dto";
 
 export class AuthRepositoryImpl implements AuthRepository {
   constructor(private readonly authDatasource: AuthDatasource) {}
+  login(loginUserDto: LoginUserDto): Promise<UserEntity> {
+    return this.authDatasource.login(loginUserDto);
+  }
 
   register(registerUserDto: RegisterUserDto): Promise<UserEntity> {
     return this.authDatasource.register(registerUserDto);
